1f446e9952eb3ea3bd2f8122863a5a0bdca863f7
joe42go <jchung79@hotmail.com>
2019-10-29 12:54:00 -0700

    completed assignment 1

---
+++ controllers/MenuController.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+
+ module.exports = class MenuController {
+   constructor(){
+     this.mainMenuQuestions = [
+      {
+       type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "getDate",
+          "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+   }
+
+   main(){
+     console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+           this.addContact();
+           break;
+         case "getDate":
+           this.getDate();
+           break;
+         case "Exit":
+           this.exit();
+         default:
+           console.log("Invalid input");
+           this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+   }
+
+   clear(){
+      console.log("\x1Bc");
+   }
+
+   addContact(){
+     this.clear();
+     console.log('addContact called');
+     this.main();
+   }
+
+   getDate(){
+     this.clear();
+     let currentDate = new Date();
+     console.log(currentDate);
+     this.main();
+   }
+
+   exit(){
+     console.log("Thanks for using AddressBloc!");
+     process.exit();
+   }
+ }
