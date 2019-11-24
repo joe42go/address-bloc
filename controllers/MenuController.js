8311a124df61322431bb708a99647e306fd4ef1e
joe42go <jchung79@hotmail.com>
2019-11-23 16:13:08 -0800

    completed creating assignment

---
+++ controllers/MenuController.js
@@ -0,0 +1,75 @@
+const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
+
+ module.exports = class MenuController {
+   constructor(){
+     this.mainMenuQuestions = [
+      {
+       type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "getDate",
+          "Exit"
+        ]
+      }
+    ];
+    this.book = new ContactController();
+   }
+
+   main(){
+     console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+           this.addContact();
+           break;
+         case "getDate":
+           this.getDate();
+           break;
+         case "Exit":
+           this.exit();
+         default:
+           console.log("Invalid input");
+           this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+   }
+
+   clear(){
+      console.log("\x1Bc");
+   }
+
+   addContact(){
+     this.clear();
+     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+       this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
+         console.log("Contact added successfully!");
+         this.main();
+       }).catch((err) => {
+         console.log(err);
+         this.main();
+       });
+     });
+   }
+
+   getDate(){
+     this.clear();
+     let currentDate = new Date();
+     console.log(currentDate);
+     this.main();
+   }
+
+   exit(){
+     console.log("Thanks for using AddressBloc!");
+     process.exit();
+   }
+
+   getContactCount(){
+     return this.contacts.length;
+   }
+ }
---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,8 @@
+const MenuController = require("../controllers/MenuController")
+
+describe("MenuController", () => {
+
+  beforeEach(() => {
+      this.menu = new MenuController();
+    });
+ });
